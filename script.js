ff7f2f4a6950f23d50f7de19a30bcf1379c5bc64
sehar batool <bsehar912@gmail.com>
2022-03-31 20:47:51 +0500

    my initial Js projects

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById('lbsInput').addEventListener('input', function(e){
+
+    let lbs= e.target.value;
+
+
+    document.getElementById('gramsOutput').innerHTML = lbs/0.0022046;
+    document.getElementById('kgOutput').innerHTML = lbs/2.2046;
+    document.getElementById('ozOutput').innerHTML = lbs*16;
+    
+});
